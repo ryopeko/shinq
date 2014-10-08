45075a6c943acae83d6aed8d3b6317dffeb1d1d7
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2014-10-08 18:14:58 +0900

    create connection

--- lib/shinq.rb
+++ lib/shinq.rb
@@ -1,11 +1,17 @@
 require "shinq/version"
+require "mysql2"
+require "shinq/client"
 
 module Shinq
   def self.configure_server
     yield self
   end
 
-  def self.q4m_connection(conn)
-    @q4m = conn
+  def self.q4m_config=(config)
+    @q4m_config = config
+  end
+
+  def self.q4m_config
+    @q4m_config
   end
 end
--- lib/shinq/client.rb
+++ lib/shinq/client.rb
@@ -1,4 +1,16 @@
+require 'json'
+require 'mysql2'
+
 module Shinq
   class Client
+    class << self
+      def enqueue(item)
+
+      end
+    end
+
+    def initialize
+      @conn ||= Mysql2::Client.new(Shinq.q4m_config)
+    end
   end
 end
