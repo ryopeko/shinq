0e8eb68cc55a8303e90ffd26b88441d795644e38
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2014-10-12 02:43:59 +0900

    Remove methods

--- lib/shinq.rb
+++ lib/shinq.rb
@@ -1,20 +1,7 @@
 require "mysql2"
-require "shinq/client"
 
 module Shinq
   VERSION = Gem.loaded_specs['shinq'].version.to_s
-
-  def self.configure_server
-    yield self
-  end
-
-  def self.q4m_config=(config)
-    @q4m_config = config
-  end
-
-  def self.q4m_config
-    @q4m_config
-  end
 
   def self.setup_db_connection(db_config)
     @conn = Mysql2::Client.new(db_config)
