b929afdbee1c221b269d332aeb09d052a0d493d7
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2014-10-17 22:48:29 +0900

    Add SimpleCov settings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,13 @@
 require 'rspec/mocks/standalone'
+require 'simplecov'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter
+]
+
+SimpleCov.start do
+  add_filter 'spec'
+end
 
 RSpec.configure do |config|
 end
