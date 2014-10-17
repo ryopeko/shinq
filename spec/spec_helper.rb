443b0d73c31b023fa15c2275adbd1ffb075ebab3
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2014-10-17 22:49:05 +0900

    Add sepc settings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,4 +10,14 @@ SimpleCov.start do
 end
 
 RSpec.configure do |config|
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
+  if config.files_to_run.one?
+    config.full_backtrace = true
+    config.default_formatter = 'doc'
+  end
+
+  config.order = :random
+  Kernel.srand config.seed
 end
