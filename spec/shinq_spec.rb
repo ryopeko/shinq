5e6f9798026626c9caa5bd4630da3b443caf5176
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2014-10-18 19:17:27 +0900

    Add Shinq.configuration specs

---
+++ spec/shinq_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+require 'shinq'
+
+describe Shinq do
+  describe ".configuration" do
+    subject { Shinq }
+
+    it { is_expected.to respond_to(:configuration) }
+
+    context "when configuration is not present" do
+      let(:shinq) { Shinq }
+
+      it { expect(shinq.configuration).to be_a_kind_of(Shinq::Configuration) }
+    end
+
+    context "when configuration is present" do
+      before do
+        Shinq.configuration = Hash.new
+      end
+
+      let(:shinq) { Shinq }
+
+      it { expect(shinq.configuration).to be_a_kind_of(Hash) }
+    end
+  end
+end
