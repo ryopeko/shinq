a79b9c37bde30e963b32413e7f0f38208b240c76
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2014-10-19 00:44:27 +0900

    use factory and add db_config specs

--- spec/shinq_spec.rb
+++ spec/shinq_spec.rb
@@ -2,25 +2,29 @@ require 'spec_helper'
 require 'shinq'
 require 'shinq/configuration'
 
+def shinq_class
+  Shinq.dup
+end
+
 describe Shinq do
-  subject { Shinq }
+  subject { shinq_class }
 
   it { is_expected.to respond_to(:configuration) }
   it { is_expected.to respond_to(:configuration=) }
 
   describe ".configuration" do
     context "when configuration is not present" do
-      let(:shinq) { Shinq }
+      let(:shinq) { shinq_class }
 
       it { expect(shinq.configuration).to be_a_kind_of(Shinq::Configuration) }
     end
 
     context "when configuration is present" do
-      before do
-        Shinq.configuration = Hash.new
-      end
-
-      let(:shinq) { Shinq }
+      let(:shinq) {
+        shinq_class.tap {|s|
+          s.configuration = Hash.new
+        }
+      }
 
       it { expect(shinq.configuration).to be_a_kind_of(Hash) }
     end
@@ -28,13 +32,25 @@ describe Shinq do
 
   describe ".configuration=" do
     context "when specify args" do
-      let(:shinq) { Shinq }
+      let(:shinq) { shinq_class }
       let(:args) { Hash.new }
 
       it 'is expect to return specified args' do
         expect(shinq.configuration=(args)).to eq args
       end
-      it { expect(shinq.configuration).to eq args }
+    end
+  end
+
+  describe ".db_config" do
+    let(:db_config) { Hash.new }
+    let(:shinq) {
+      shinq_class.tap {|s|
+        s.configuration = Shinq::Configuration.new(db_config: db_config)
+      }
+    }
+
+    it 'is expect to return specified args' do
+      expect(shinq.db_config).to eq db_config
     end
   end
 end
