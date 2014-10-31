1f634dbdc291c95a5c4e5ec6b82ab09410f80b85
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2014-10-31 16:56:30 +0900

    Add integration spec

---
+++ spec/integration_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+require 'shinq'
+require 'shinq/client'
+
+describe "Integration" do
+  before do
+    load_database_config(Shinq)
+  end
+
+  context "When create queue" do
+    let(:queue_table) { 'queue_test' }
+    let(:args) { {title: 'foo'} }
+
+    before do
+      Shinq::Client.enqueue(
+        table_name: queue_table,
+        job_id: 'jobid',
+        args: args
+      )
+
+      @queue = Shinq::Client.dequeue(table_name: queue_table)
+      Shinq::Client.done
+    end
+
+    it { expect(@queue[:title]).to eq args[:title] }
+  end
+end
