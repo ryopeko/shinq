70c136d4499a60392336dbb939ac981f3a416bab
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2014-11-03 01:58:51 +0900

    Add create worker file

--- lib/generators/shinq/worker/worker_generator.rb
+++ lib/generators/shinq/worker/worker_generator.rb
@@ -1,6 +1,12 @@
+require 'rails/generators/named_base'
 module Shinq
   module Generators
-    class WorkerGenerator < ::Rails::Generators::Base
+    class WorkerGenerator < ::Rails::Generators::NamedBase
+      source_root File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
+
+      def create_worker_file
+        template 'worker.erb', File.join('app/workers', class_path, "#{file_name}.rb")
+      end
     end
   end
 end
