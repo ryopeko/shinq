b8b84d2e03a7ec0ac84e88e870122cdbcd7188b8
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2014-11-05 20:32:21 +0900

    Fix generator

--- lib/generators/shinq/worker/worker_generator.rb
+++ lib/generators/shinq/worker/worker_generator.rb
@@ -1,8 +1,26 @@
 require 'rails/generators/named_base'
+require 'rails/generators/model_helpers'
+require 'rails/generators/active_record/migration'
+
 module Shinq
   module Generators
     class WorkerGenerator < ::Rails::Generators::NamedBase
+      include ::Rails::Generators::ModelHelpers
+      include ::Rails::Generators::Migration
+      include ::ActiveRecord::Generators::Migration
+
+      argument :attributes, :type => :array, :default => [], :banner => "field[:type][:index] field[:type][:index]"
+
+      check_class_collision
+
+      class_option :migration,  :type => :boolean
+      class_option :parent,     :type => :string, :desc => "The parent class for the generated model"
+
       source_root File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
+
+      def create_migration_file
+        migration_template 'create_table_migration.erb', "db/migrate/create_#{table_name}.rb"
+      end
 
       def create_worker_file
         template 'worker.erb', File.join('app/workers', class_path, "#{file_name}.rb")
